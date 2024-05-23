820fa6b0cfefac2df844844d226805d75506c838
Vamsi Krishna Satyasi <vamsikrishnasatyasi971@gmail.com>
2024-05-23 16:48:45 -0700

    progress bar UI

--- lib/AVAM/Profile.dart
+++ lib/AVAM/Profile.dart
@@ -21,14 +21,23 @@ class Profilepage extends StatefulWidget {
 
 class _ProfilepageState extends State<Profilepage> {
 
-  
-  
+  final List<Activity> activities = [
+    Activity(name: "Wellness", progress: 0.70),
+    Activity(name: "Emotions", progress: 0.50),
+    Activity(name: "Stress", progress: 0.20),
+    Activity(name: "Relaxation", progress: 0.30),
+    Activity(name: "Mindfullness", progress: 0.20),
+    Activity(name: "Sleep", progress: 0.40),
+  ];
 
+  double calculateOverallProgress() {
+    double totalProgress = activities.fold(0, (sum, activity) => sum + activity.progress);
+    return totalProgress / activities.length;
+  }
 
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-
+    return Scaffold(
       body: Container(
         height: double.infinity,
         width: double.infinity,
@@ -39,29 +48,21 @@ class _ProfilepageState extends State<Profilepage> {
             fit: BoxFit.fill,
           ),
         ),
-
         child: SingleChildScrollView(
-          child:  Column(
-            //mainAxisAlignment: MainAxisAlignment.start, // Customize as needed
-            // crossAxisAlignment: CrossAxisAlignment.center, // Customize as needed
+          child: Column(
             children: <Widget>[
-              SizedBox(height: 10,),
-              //////////////////////////////////////////////////////////////
+              SizedBox(height: 10),
               Image(
-                  height: 180,
-                  width: 180,
-                  image: AssetImage('assets/logo.png')
+                height: 180,
+                width: 180,
+                image: AssetImage('assets/logo.png'),
               ),
-              //////////////////////////////////////////////////////////
-              SizedBox(height: 5,),
-              ///////////////////////////////////////////////////////////////////////
+              SizedBox(height: 5),
               Container(
                 alignment: Alignment.center,
                 height: 100,
                 width: 340,
-
                 decoration: BoxDecoration(
-
                   color: Colors.tealAccent.withOpacity(0.4),
                   borderRadius: BorderRadius.circular(20.0),
                   border: Border.all(
@@ -77,70 +78,62 @@ class _ProfilepageState extends State<Profilepage> {
                     ),
                   ],
                 ),
-
                 child: Row(
-
                   mainAxisAlignment: MainAxisAlignment.center,
                   crossAxisAlignment: CrossAxisAlignment.center,
-
                   children: <Widget>[
-
-                    SizedBox(
-                      width: 10,
-                    ),
-
+                    SizedBox(width: 10),
                     CircleAvatar(
-
                       radius: 35.0,
-                      backgroundColor: Colors.black.withOpacity(0.2), // Background color of the circle
+                      backgroundColor: Colors.black.withOpacity(0.2),
                       child: Icon(
                         Icons.person,
                         size: 55.0,
-                        color: Colors.white, // Color of the person icon
+                        color: Colors.white,
                       ),
                     ),
-
-                    SizedBox(
-                      width: 25,
-                    ),
-
+                    SizedBox(width: 25),
                     Column(
-
                       mainAxisAlignment: MainAxisAlignment.start,
                       crossAxisAlignment: CrossAxisAlignment.center,
-
                       children: <Widget>[
-
                         SizedBox(height: 8.0),
                         Text(
-                          'Edison Nalluri', // Replace with the user's name
-                          style: TextStyle(fontSize: 18.0,),
+                          'Edison Nalluri',
+                          style: TextStyle(fontSize: 18.0),
                         ),
-
                         SizedBox(height: 5.0),
                         Text(
-                          'edison@gmail.com', // Replace with the user's email
+                          'edison@gmail.com',
                           style: TextStyle(fontSize: 18.0, color: Colors.black),
                         ),
-
                         SizedBox(height: 5.0),
                         Text(
-                          '24', // Replace with the user's email
+                          '24',
                           style: TextStyle(fontSize: 18.0, color: Colors.black),
                         ),
-
                       ],
                     ),
                   ],
                 ),
               ),
-              ////////////////////////////////////////////////////////////////////////
-              SizedBox(height: 340,),
-              //////////////////////////////////////////////////////////////////////
+              SizedBox(height: 30),
+              LinearProgressIndicator(
+                value: calculateOverallProgress(),
+                backgroundColor: Colors.black,
+                valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+              ),
 
+              ListView.builder(
+                shrinkWrap: true,
+                physics: NeverScrollableScrollPhysics(),
+                itemCount: activities.length,
+                itemBuilder: (context, index) {
+                  return ActivityCard(activity: activities[index]);
+                },
+              ),
 
-
-
+              SizedBox(height: 40),
               Container(
                 alignment: Alignment.center,
                 height: 60,
@@ -173,8 +166,8 @@ class _ProfilepageState extends State<Profilepage> {
                       Text(
                         'Logout',
                         style: TextStyle(
-                            fontSize: 20,
-                            color: Colors.white,
+                          fontSize: 20,
+                          color: Colors.white,
                         ),
                       ),
                     ],
@@ -184,15 +177,13 @@ class _ProfilepageState extends State<Profilepage> {
                 ),
 
               ),
-
             ],
           ),
         ),
-
       ),
-
     );
   }
+
 }
 
 class MyCustomIcons {
@@ -211,4 +202,55 @@ class MyCustomIcons {
   static const IconData featured_video_outlined = IconData(0xf04d, fontFamily: 'MaterialIcons', matchTextDirection: true);
   static const IconData account_circle = IconData(0xe043, fontFamily: 'MaterialIcons');
 
+}
+
+
+
+
+class Activity {
+  final String name;
+  final double progress;
+
+  Activity({required this.name, required this.progress});
+}
+
+class ActivityCard extends StatelessWidget {
+  final Activity activity;
+
+  const ActivityCard({required this.activity});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color: Colors.tealAccent.withOpacity(0.7),
+      margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 26),
+      child: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Row(
+          children: [
+            CircularProgressIndicator(
+              value: activity.progress,
+              backgroundColor: Colors.black,
+              valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+            ),
+            SizedBox(width: 20),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  activity.name,
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color:Colors.white ),
+                ),
+                SizedBox(height: 10),
+                Text(
+                  '${(activity.progress * 100).toStringAsFixed(0)}%',
+                  style: TextStyle(fontSize: 16, color:Colors.white),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
